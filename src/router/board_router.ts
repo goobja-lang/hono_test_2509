1035669a5a4266d6d130ce367219616ec23fba1a
goobja-lang <goobja@naver.com>
2025-10-21 11:46:07 +0900

    .

--- src/router/board_router.ts
+++ src/router/board_router.ts
@@ -28,7 +28,42 @@ router.get("/get_memo_list", async (c) => {
   };
   try {
     const boardRepo = AppDataSource.getRepository(TBoard);
-    const memo = await boardRepo.find();
+    /*
+    SELECT
+    *
+    FROM t_board
+    ORDER BY created_dt DESC
+    LIMIT 1000
+     */
+    const memo = await boardRepo.find({
+      order: { createdDt: "DESC" },
+      take: 1000,
+    });
+    result.data = memo;
+    return c.json(result);
+  } catch (error: any) {
+    result.success = false;
+    result.msg = `server error. ${error?.message ?? ""}`;
+    return c.json(result);
+  }
+});
+
+router.get("/get_memo", async (c) => {
+  let result: { success: boolean; data: any; msg: string } = {
+    success: true,
+    data: null,
+    msg: ``,
+  };
+  try {
+    let id = Number(c?.req?.query("id") ?? 0);
+    const boardRepo = AppDataSource.getRepository(TBoard);
+    /*
+    SELECT
+    *
+    FROM t_baord
+    WHERE id=1
+    */
+    const memo = await boardRepo.findOne({ where: { id: id } });
     result.data = memo;
     return c.json(result);
   } catch (error: any) {
@@ -46,12 +81,14 @@ router.post("/upsert", async (c) => {
   };
   try {
     const body = await c?.req?.parseBody();
+    let id = Number(body["id"] ?? 0);
     let title = String(body["title"]);
     let content = String(body["content"]);
 
     /* t_board 에 데이터 저장하기 */
     const boardRepo = AppDataSource.getRepository(TBoard);
-    let newBoard = new TBoard();
+    let newBoard =
+      (await boardRepo.findOne({ where: { id: id } })) ?? new TBoard();
     newBoard.title = title;
     newBoard.content = content;
     newBoard = await boardRepo.save(newBoard);
